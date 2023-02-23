19aa5d2c3007881bdadfe06144012dc66246074d
lipkij <lipkij@gmail.com>
2023-02-24 01:22:20 +0400

    Binary guess search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
+    getMid() {
+        return Math.round((this.min + this.max) / 2);
     }
 
     guess() {
-
+        return this.getMid();
     }
 
     lower() {
-
+        this.max = this.getMid();
     }
 
     greater() {
-
+        this.min = this.getMid();
     }
 }
 
